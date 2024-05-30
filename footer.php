fbe5c8b6709dfe189cb19aa01b51e36b3b48cb20
Jamila BOUMEHDI <mabchourjamila87@gmail.com>
2024-05-31 01:08:05 +0200

    créer le footer du thème + gestion du Menu footer admin  Wordpress

--- footer.php
+++ footer.php
@@ -1,6 +1,17 @@
-<footer>
-        <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
-    </footer>
-    <?php wp_footer(); ?>
-</body>
+        <footer class="footer">
+            <div class="footer__links">
+                <?php
+                wp_nav_menu(array(
+                    'theme_location' => 'footer-menu',
+                    'menu_class' => 'footer__menu',
+                    'container' => false,
+                    'depth' => 1,
+                    'fallback_cb' => false,
+                ));
+                ?>
+            </div>
+            <div class="footer__text">Tous droits réservés</div>
+        </footer>
+        <?php wp_footer(); ?>
+    </body>
 </html>
--- functions.php
+++ functions.php
@@ -49,4 +49,13 @@ function motaphoto_widgets_init() {
     ));
 }
 add_action('widgets_init', 'motaphoto_widgets_init');
+
+function register_footer_menu() {
+    register_nav_menu('footer-menu', __('Footer Menu'));
+}
+add_action('init', 'register_footer_menu');
+
+
+
+
 ?>
