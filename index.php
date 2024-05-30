5ac441e6424d73bc5f997e6596b0fa85a4026752
Jamila BOUMEHDI <mabchourjamila87@gmail.com>
2024-05-30 14:01:32 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Theme Name: Motaphoto
+Theme URI: http://motaphoto.com
+Author: Jamila BOUMEHDI
+Author URI: http://jamila-boumehdi.com
+Description: Thème WordPress pour la photographie Nathalie MOTA.
+Version: 1.0
+License: GNU General Public License v2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: motaphoto
+*/
