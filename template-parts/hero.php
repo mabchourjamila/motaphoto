3eefd97067ff72317f667b393efded74b4a2a3c4
Jamila BOUMEHDI <mabchourjamila87@gmail.com>
2024-06-23 14:39:45 +0200

    template part pour section HERO de la pages d'accueil

---
+++ template-parts/hero.php
@@ -0,0 +1,23 @@
+<div class="hero">
+    <h1>Photographe Event</h1>
+    <div class="photo-random">
+        <?php
+        // Affichage aléatoire d'une photo
+        $args = array(
+            'post_type' => 'photographies',
+            'posts_per_page' => 1,
+            'orderby' => 'rand',
+        );
+
+        $photo_aleatoire_hero = new WP_Query($args);
+
+        if ($photo_aleatoire_hero->have_posts()) {
+            while ($photo_aleatoire_hero->have_posts()) {
+                $photo_aleatoire_hero->the_post();
+                the_content();
+            }
+            wp_reset_postdata();
+        }
+        ?>
+    </div>
+</div>
