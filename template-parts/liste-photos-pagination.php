e06860d9b33ae0a8465246a9456f8db533abf8d7
Jamila BOUMEHDI <mabchourjamila87@gmail.com>
2024-06-23 14:40:20 +0200

    template part pour la liste des photos avec pagination de la pages d'accueil

---
+++ template-parts/liste-photos-pagination.php
@@ -0,0 +1,29 @@
+<div class="affichage-des-photos">
+    <div class="zone-les-photos">
+        <!-- Création d'une loop pour afficher toutes les photos -->
+        <?php
+        $args = array(
+            'post_type' => 'photographies',
+            'posts_per_page' => 8,
+            'orderby' => 'date',
+            'order' => 'ASC',
+            'paged' => 1,
+        );
+
+        $photo_query = new WP_Query($args);
+
+        if ($photo_query->have_posts()) {
+            while ($photo_query->have_posts()) {
+                $photo_query->the_post();
+                get_template_part('template-parts/photo-bloc');
+            }
+            wp_reset_postdata();
+        } else {
+            echo 'Aucune photo trouvée.';
+        }
+        ?>
+    </div>
+</div>
+<div class="bouton-accueil">
+    <button id="charger-plus" class="voir-plus">Charger plus</button>
+</div>
